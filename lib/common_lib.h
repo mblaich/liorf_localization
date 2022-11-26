eb3fd9c1b4c4ca290b6dbcb0ac652b4cbb90ecb6
YJZLuckyBoy <jason_zhao2022@163.com>
2022-11-27 00:07:10 +0800

    1. first version

---
+++ lib/common_lib.h
@@ -0,0 +1,19 @@
+#ifndef _COMMON_LIB_H_
+#define _COMMON_LIB_H_
+#include <iostream>
+#include <string>
+namespace CommonLib
+{
+  class common_lib
+  {
+    public:
+      common_lib();
+      ~common_lib();
+
+      void pointDistance();
+      void PKG_VERSION();
+  };
+} // namespace CommonLib
+
+
+#endif
